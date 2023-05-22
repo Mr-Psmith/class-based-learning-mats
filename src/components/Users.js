10ccc1961a322e8ad944c40c2e2c468a4f7c7dcb
Mr-Psmith <psancthus@gmail.com>
2023-05-22 17:51:23 +0000

    added some stuff and some errors, tomorrow will finish

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,15 +1,45 @@
-import { useState } from 'react';
+import { Component, useState } from 'react';
 import User from './User';
 
 import classes from './Users.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
+class Users extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showUsers: true
+    };
+  }
+  toggleUsersHandler() {
+    /* this.state.showUsers = false; //Not! */
+    this.setState((curState) => {
+      return { showUsers: !curState.showUsers }
+    });
+  };
 
-const Users = () => {
+
+  render() {
+  const usersList = (
+    <ul>
+      {this.props.users.map((user) => (
+        <User key={user.id} name={user.name} />
+      ))}
+    </ul>
+  );
+
+  return (
+    <div className={classes.users}>
+      <button onClick={this.toggleUsersHandler.bind(this)}>
+        {this.state.showUsers ? 'Hide' : 'Show'} Users
+      </button>
+      {this.state.showUsers && usersList}
+    </div>
+  );
+  }
+}
+
+
+/* const Users = () => {
   const [showUsers, setShowUsers] = useState(true);
 
   const toggleUsersHandler = () => {
@@ -32,6 +62,6 @@ const Users = () => {
       {showUsers && usersList}
     </div>
   );
-};
+}; */
 
 export default Users;
---
+++ src/components/user-finder.js
@@ -0,0 +1,42 @@
+import { Component, Fragment, useState, useEffect } from 'react';
+
+import Users from './Users';
+import classes from './user-finder.module.css';
+
+const DUMMY_USERS = [
+  { id: 'u1', name: 'Max' },
+  { id: 'u2', name: 'Manuel' },
+  { id: 'u3', name: 'Julie' },
+];
+
+class UserFinder extends Component {
+    
+}
+
+
+
+const UserFinder = () => {
+  const [filteredUsers, setFilteredUsers] = useState(DUMMY_USERS);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    setFilteredUsers(
+      DUMMY_USERS.filter((user) => user.name.includes(searchTerm))
+    );
+  }, [searchTerm]);
+
+  const searchChangeHandler = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  return (
+    <Fragment>
+      <div className={classes.finder}>
+        <input type='search' onChange={searchChangeHandler} />
+      </div>
+      <Users users={filteredUsers} />
+    </Fragment>
+  );
+};
+
+export default UserFinder;
